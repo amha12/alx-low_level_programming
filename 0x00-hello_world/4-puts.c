cd61e2f1af49af24271f19948b3431dbe3a20912
amha12 <amhasilse12@gmail.com>
2022-12-10 11:40:17 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
